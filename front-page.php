99f8e19e4895ddca6222841afcd28d67601ca721
Aditya Jain <adityabhooshanjain@gmail.com>
2023-07-21 11:56:55 +0530

    home-page-temp-tweak

--- front-page.php
+++ front-page.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
 <main>
+	<section class="bg-dark text-center locations-container">
+		<div class="container container--narrow">
+			<p>Khoj &sol; Home</p>
+			<h2>Khoj</h2>
+		</div>
+	</section>
 	<?php the_content(); ?>
 </main>
 
