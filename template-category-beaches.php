7593716c6a169de1080e1174310c0733839599c1
Aditya Jain <adityabhooshanjain@gmail.com>
2023-07-24 10:17:13 +0530

    all-category-borders-added

---
+++ template-category-beaches.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Coast and Beaches Category
+Template Post Type: page
+*/
+?>
+
+<?php
+$args = array('category_name' => 'coast-and-beaches');
+$the_query = new WP_Query($args);
+?>
+
+<?php get_header(); ?>
+
+<main>
+    <section class="bg-dark text-center category coast-and-beaches">
+        <div class="container container--narrow">
+            <p>Khoj &sol; Coast And Beaches</p>
+            <h2>Coast And Beaches</h2>
+        </div>
+    </section>
+    <?php if ($the_query->have_posts()) : ?>
+        <div class="category-container">
+            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <a href="<?php the_permalink(); ?>">
+                    <div class="category-card stacked">
+                        <?php the_post_thumbnail(); ?>
+                        <div class="category-content">
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </div>
+                </a>
+            <?php endwhile; ?>
+        <?php endif; ?>
+        </div>
+        <?php wp_reset_query(); ?>
+</main>
+
+<?php get_footer(); ?>
---
+++ template-category-culture.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Culture And Religion Category
+Template Post Type: page
+*/
+?>
+
+<?php
+$args = array('category_name' => 'culture-and-religion');
+$the_query = new WP_Query($args);
+?>
+
+<?php get_header(); ?>
+
+<main>
+    <section class="bg-dark text-center category culture-and-religion">
+        <div class="container container--narrow">
+            <p>Khoj &sol; Culture And Religion</p>
+            <h2>Culture And Religion</h2>
+        </div>
+    </section>
+    <?php if ($the_query->have_posts()) : ?>
+        <div class="category-container">
+            <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+                <a href="<?php the_permalink(); ?>">
+                    <div class="category-card stacked">
+                        <?php the_post_thumbnail(); ?>
+                        <div class="category-content">
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </div>
+                </a>
+            <?php endwhile; ?>
+        <?php endif; ?>
+        </div>
+        <?php wp_reset_query(); ?>
+</main>
+
+<?php get_footer(); ?>
