d52eb0477636be66658c46ced8849f2e62df0ed7
Aditya Jain <adityabhooshanjain@gmail.com>
2023-08-23 22:34:37 +0100

    faq-and-pp-page-1.3.6-update

---
+++ faq-page.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Template Name: FAQ Page
+Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<main>
+    <section class="bg-dark text-center locations-container">
+        <div class="container container--narrow">
+            <p>Khoj &sol; FAQ</p>
+            <h2>FAQ</h2>
+        </div>
+    </section>
+    <section class="bg-white about-section">
+        <div class="container">
+            <p>Welcome to the FAQ page of Khoj! We're here to address common questions you might have about our website, its purpose, features, and how to make the most of your experience.</p>
+            <dl>
+                <dt>1. What is Khoj?</dt>
+                <dd>Khoj is a website dedicated to helping you discover the hidden gems of India beyond what is commonly found on the web. We curate content that showcases lesser-known destinations and cultural experiences that make India truly extraordinary.</dd>
+                <dt>2. Who is behind Khoj?</dt>
+                <dd>Khoj is a passion project by Adi, developed as the major project for M.A Web Design and Content Planning at the University of Greenwich, London. He is a passionate traveler and explorer with a deep love for India's diverse culture and heritage. You can find Adi at <a href="https://www.linkedin.com/in/aditya-jain26" target="_blank">LinkedIn</a>.</dd>
+                <dt>3. How is Khoj different from other travel websites?</dt>
+                <dd>While many travel websites focus on popular tourist destinations, Khoj aims to bring attention to the lesser-known and hidden treasures of India. Our content is carefully curated to provide authentic experiences.</dd>
+                <dt>4. Can I contribute my own travel experiences to Khoj?</dt>
+                <dd>Absolutely! We encourage our community of users to contribute their own stories, photos, and insights about hidden gems in India. Sharing your experiences enriches the platform and helps fellow travelers explore unique places. Best place so reach out is at <a href="mailto:aditya@khoj-india.com">aditya@khoj-india.com</a></dd>
+                <dt>5. How do I navigate the website?</dt>
+                <dd>The Khoj website is designed to be user-friendly. You can explore different sections of Locations like "Natural Landscapes," "Cultural and Religion", "Lakes and Waterfalls" and more from the navigation menu. Click on the locations that interest you to dive into the information about them, as well as how to get there.</dd>
+                <dt>6. Is Khoj affiliated with any travel companies or organizations?</dt>
+                <dd>No, Khoj is an independent project developed with a passion for exploration. We're not affiliated with any travel companies or organizations.</dd>
+                <dt>7. How can I support Khoj?</dt>
+                <dd>Supporting Khoj is simple! You can engage with our content, share it with fellow travelers, and contribute your own experiences. Your participation helps build a vibrant community of explorers.</dd>
+            </dl>
+            <p>If you have any other questions or need further assistance, don't hesitate to contact us. Thank you for being a part of the Khoj journey!</p>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
---
+++ privacy-policy-page.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Template Name: Privacy Policy Page
+Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<main>
+    <section class="bg-dark text-center locations-container">
+        <div class="container container--narrow">
+            <p>Khoj &sol; Privacy Policy</p>
+            <h2>Privacy Policy</h2>
+        </div>
+    </section>
+    <section class="bg-white about-section">
+        <div class="container">
+            <p>Effective Date: 23 August 2023</p>
+            <p>Thank you for using Khoj! This Privacy Policy outlines how we collect and utilize information through the use of Google Analytics on our website. Your privacy is of utmost importance to us, and we want to ensure that you are aware of how data is handled when you interact with Khoj.</p>
+            <dl>
+                <dt>1. Information Collected by Google Analytics:</dt>
+                <dd>We use Google Analytics, a web analytics service provided by Google, to gather information about how users interact with our website. Google Analytics collects data such as your IP address, browser type, device type, pages visited, time spent on pages, and other relevant browsing information.</dd>
+                <dt>2. Purpose of Data Collection:</dt>
+                <dd>The data collected through Google Analytics is used to analyze website traffic, user behavior, and engagement. This helps us understand how visitors use Khoj and enables us to improve user experience and content quality.</dd>
+                <dt>3. Cookies and Data Retention:</dt>
+                <dd>Google Analytics employs cookies, which are small text files placed on your device, to collect data. These cookies store anonymous information about your interactions with the website. The data collected is retained for a specified period in Google Analytics, but we do not have control over the exact retention period.</dd>
+                <dt>4. Data Sharing:</dt>
+                <dd>We do not share personally identifiable information with Google Analytics. The collected data is used solely for internal analytical purposes.</dd>
+                <dt>5. Opting Out:</dt>
+                <dd>If you do not wish to have your data collected and used by Google Analytics, you can install the Google Analytics <a href="https://tools.google.com/dlpage/gaoptout/">Opt-out Browser Add-on</a>. This will prevent Google Analytics from collecting data about your visits to websites that use the service.</dd>
+                <dt>6. Third-Party Links:</dt>
+                <dd>Khoj may contain links to third-party websites. Please be aware that this Privacy Policy applies solely to Khoj and the information collected through Google Analytics. We are not responsible for the privacy practices of external websites.</dd>
+                <dt>7. Changes to Privacy Policy:</dt>
+                <dd>We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. The most current version will be posted on this page with the effective date.</dd>
+                <dt>8. Contact Information:</dt>
+                <dd>If you have any questions or concerns about this Privacy Policy or your data, please contact us at <a href="mailto:aditya@khoj-india.com">aditya@khoj-india.com</a></dd>
+            </dl>
+            <p>By using Khoj, you acknowledge that you have read, understood, and agreed to be bound by this Privacy Policy in relation to the collection and use of data through Google Analytics. Thank you for trusting us with your information, and we are committed to ensuring a transparent and secure browsing experience on our website.</p>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
