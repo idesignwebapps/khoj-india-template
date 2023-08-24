cdf76be510af018c3ab1ebbb3a1c11069972fc80
Aditya Jain <adityabhooshanjain@gmail.com>
2023-08-24 16:12:05 +0100

    added-404-page-1.3.9

--- 404.php
+++ 404.php
@@ -1,9 +1,20 @@
 <?php get_header(); ?>
-<!-- content START -->
-<div id="content">
-	<article>		<h2>Not Found (error 404)</h2>		<p>Sorry, but you are looking for something that isn't here.</p>		<?php include (TEMPLATEPATH . "/searchform.php"); ?>	</article>
-</div>
-<!-- content END -->
-<?php get_sidebar(); ?>
+
+<main>
+	<section class="bg-dark text-center locations-container">
+		<div class="container container--narrow">
+			<p>Khoj &sol; Error</p>
+			<h2>404 Error</h2>
+		</div>
+	</section>
+	<section class="bg-white about-section">
+		<div class="container">
+			<h2>404 Error</h2>
+			<p>The page you are looking for does not exists</p>
+			<p>Something has gone wrong and the page you're looking for can't be found.</p>
+			<p>You could visit <a href="<?php echo get_home_url(); ?>">the Homepage</a> to navigate to what you're looking for.</p>
+		</div>
+	</section>
+</main>
 
 <?php get_footer(); ?>
