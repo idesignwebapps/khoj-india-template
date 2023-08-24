1b1700c4d6190fd946d08f52b255c25305442f61
Aditya Jain <adityabhooshanjain@gmail.com>
2023-08-24 03:28:58 +0100

    added-interactive-map-1.3.8

---
+++ interactive-map-page.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Interactive Map Page
+Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<main>
+    <section class="bg-dark text-center locations-container">
+        <div class="container container--narrow">
+            <p>Khoj &sol; Interactive Map</p>
+            <h2>Interactive Map</h2>
+        </div>
+    </section>
+    <section class="bg-white about-section">
+        <div class="container">
+            <iframe src="https://www.google.com/maps/d/embed?mid=1uYTku_MAlGWQKTIJX2PvPOvk4NwFlvU&ehbc=2E312F&noprof=1"></iframe>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
