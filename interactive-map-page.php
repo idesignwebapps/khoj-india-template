aedc2189a8bd8fe04ae7acf5c60b53030223da57
Aditya Jain <adityabhooshanjain@gmail.com>
2023-08-24 10:06:34 +0100

    interactive-map-hotfix

--- interactive-map-page.php
+++ interactive-map-page.php
@@ -16,6 +16,7 @@ Template Post Type: page
     </section>
     <section class="bg-white about-section">
         <div class="container">
+            <p>Interactive Map marks all of the carefully curated locations on the website. Use the toggle on the top left of the map to view the locations or filter them by category.</p>
             <iframe src="https://www.google.com/maps/d/embed?mid=1uYTku_MAlGWQKTIJX2PvPOvk4NwFlvU&ehbc=2E312F&noprof=1"></iframe>
         </div>
     </section>
