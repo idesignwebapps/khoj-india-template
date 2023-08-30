b5cd6f2a9b6c1f699b254f3f2a99baf6597a070a
Aditya Jain <adityabhooshanjain@gmail.com>
2023-08-30 12:16:29 +0100

    updated-home

--- front-page.php
+++ front-page.php
@@ -1,3 +1,10 @@
+<?php
+/*
+Template Name: Home Page
+Template Post Type: page
+*/
+?>
+
 <?php get_header(); ?>
 
 <main>
@@ -7,7 +14,13 @@
 			<h2>Khoj</h2>
 		</div>
 	</section>
-	<?php the_content(); ?>
+	<section class="bg-white about-section">
+		<div class="container">
+			<p>India, with its rich cultural heritage, diverse landscapes, and historical marvels, is a treasure trove of stunning destinations waiting to be explored. This website, is a part of Major Project submission for M.A. Web Design and Content Planning by Adi, and is currently under developement.</p>
+			<p>This website that will take you on a virtual journey to some of the most beautiful hidden gem of locations across the country.</p>
+			<p>Meanwhile, have fun looking at the progess! If you have any questions or suggestions, feel free to reach out to me at <a href="mailto:aditya@khoj-india.com">aditya@khoj-india.com</a> or at <a href="https://www.linkedin.com/in/aditya-jain26" target="_blank">LinkedIn</a>. I would love to hear from you!</p>
+		</div>
+	</section>
 </main>
 
 <?php get_footer(); ?>
