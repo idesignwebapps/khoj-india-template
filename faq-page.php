9499be8f339af58e606ef2831962da9d55091c08
Aditya Jain <adityabhooshanjain@gmail.com>
2023-09-10 07:18:04 +0100

    updated-question

--- faq-page.php
+++ faq-page.php
@@ -22,15 +22,17 @@ Template Post Type: page
                 <dd>Khoj is a website dedicated to helping you discover the hidden gems of India beyond what is commonly found on the web. We curate content that showcases lesser-known destinations and cultural experiences that make India truly extraordinary.</dd>
                 <dt>2. Who is behind Khoj?</dt>
                 <dd>Khoj is a passion project by Adi, developed as the major project for M.A Web Design and Content Planning at the University of Greenwich, London. He is a passionate traveler and explorer with a deep love for India's diverse culture and heritage. You can find Adi at <a href="https://www.linkedin.com/in/aditya-jain26" target="_blank">LinkedIn</a>.</dd>
-                <dt>3. How is Khoj different from other travel websites?</dt>
+                <dt>3. What is the meaning of Khoj?</dt>
+                <dd>"Khoj" is a Hindi word that means "search" or "discovery" in English. It can also refer to a quest or exploration for knowledge or truth.</dd>
+                <dt>4. How is Khoj different from other travel websites?</dt>
                 <dd>While many travel websites focus on popular tourist destinations, Khoj aims to bring attention to the lesser-known and hidden treasures of India. Our content is carefully curated to provide authentic experiences.</dd>
-                <dt>4. Can I contribute my own travel experiences to Khoj?</dt>
+                <dt>5. Can I contribute my own travel experiences to Khoj?</dt>
                 <dd>Absolutely! We encourage our community of users to contribute their own stories, photos, and insights about hidden gems in India. Sharing your experiences enriches the platform and helps fellow travelers explore unique places. Best place so reach out is at <a href="mailto:aditya@khoj-india.com">aditya@khoj-india.com</a></dd>
-                <dt>5. How do I navigate the website?</dt>
+                <dt>6. How do I navigate the website?</dt>
                 <dd>The Khoj website is designed to be user-friendly. You can explore different sections of Locations like "Natural Landscapes," "Cultural and Religion", "Lakes and Waterfalls" and more from the navigation menu. Click on the locations that interest you to dive into the information about them, as well as how to get there.</dd>
-                <dt>6. Is Khoj affiliated with any travel companies or organizations?</dt>
+                <dt>7. Is Khoj affiliated with any travel companies or organizations?</dt>
                 <dd>No, Khoj is an independent project developed with a passion for exploration. We're not affiliated with any travel companies or organizations.</dd>
-                <dt>7. How can I support Khoj?</dt>
+                <dt>8. How can I support Khoj?</dt>
                 <dd>Supporting Khoj is simple! You can engage with our content, share it with fellow travelers, and contribute your own experiences. Your participation helps build a vibrant community of explorers.</dd>
             </dl>
             <p>If you have any other questions or need further assistance, don't hesitate to contact us. Thank you for being a part of the Khoj journey!</p>
