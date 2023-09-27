eac16e328b8b569321e1a7e53f1f3d9365f0c87a
Aditya Jain <adityabhooshanjain@gmail.com>
2023-09-28 00:41:38 +0100

    indentation-hotfix

--- single-location-page.php
+++ single-location-page.php
@@ -16,12 +16,8 @@ Template Post Type: post
 		<!-- hero image -->
 		<img src="<?php echo esc_url($hero['featured-image']['url']); ?>" alt="<?php echo esc_attr($hero['featured-image']['alt']); ?>">
 		<div class="container container--narrow">
-			<p>
-				<?php echo $hero['location-state']; ?>
-			</p>
-			<h2>
-				<?php echo $hero['location-title']; ?>
-			</h2>
+			<p><?php echo $hero['location-state']; ?></p>
+			<h2><?php echo $hero['location-title']; ?></h2>
 			<a href="#travel">Get Me There <img src="<?php echo get_template_directory_uri(); ?>/images/location.png" alt=""></a>
 		</div>
 	</section>
@@ -32,24 +28,14 @@ Template Post Type: post
 		<div class="container">
 			<div class="split">
 				<div>
-					<h2>
-						<?php echo $first_section['first-sub-title']; ?>
-					</h2>
-					<p>
-						<?php echo $first_section['first-content-1']; ?>
-					</p>
-					<p>
-						<?php echo $first_section['first-content-2']; ?>
-					</p>
+					<h2><?php echo $first_section['first-sub-title']; ?></h2>
+					<p><?php echo $first_section['first-content-1']; ?></p>
+					<p><?php echo $first_section['first-content-2']; ?></p>
 				</div>
 				<div>
 					<img src="<?php echo esc_url($first_section['image-1']['url']); ?>" alt="<?php echo esc_attr($first_section['image-1']['alt']); ?>">
-					<p>
-						<?php echo $first_section['first-content-3']; ?>
-					</p>
-					<p>
-						<?php echo $first_section['first-content-4']; ?>
-					</p>
+					<p><?php echo $first_section['first-content-3']; ?></p>
+					<p><?php echo $first_section['first-content-4']; ?></p>
 				</div>
 			</div>
 		</div>
@@ -61,22 +47,16 @@ Template Post Type: post
 	<!-- second section START -->
 	<section class="bg-white">
 		<div class="container">
-			<h2 class="text-center">
-				<?php echo $second_section['second-sub-title']; ?>
-			</h2>
+			<h2 class="text-center"><?php echo $second_section['second-sub-title']; ?></h2>
 			<div class="split">
 				<div>
-					<p>
-						<?php echo $second_section['second-content-1']; ?>
-					</p>
+					<p><?php echo $second_section['second-content-1']; ?></p>
 				</div>
 				<div>
 					<img src="<?php echo esc_url($second_section['image-2']['url']); ?>" alt="<?php echo esc_attr($second_section['image-2']['alt']); ?>">
 				</div>
 				<div>
-					<p>
-						<?php echo $second_section['second-content-2']; ?>
-					</p>
+					<p><?php echo $second_section['second-content-2']; ?></p>
 				</div>
 			</div>
 		</div>
@@ -92,27 +72,16 @@ Template Post Type: post
 				<!-- getting there information -->
 				<div>
 					<h2>Getting There</h2>
-					<p><span class="material-symbols-outlined">location_city</span>
-						<?php echo $third_section['nearest-city']; ?>
-					</p>
-					<p><span class="material-symbols-outlined">flight</span>
-						<?php echo $third_section['nearest-airport']; ?>
-					</p>
-					<p><span class="material-symbols-outlined">directions_car</span>
-						<?php echo $third_section['nearest-road-distance']; ?>
-					</p>
-					<p><span class="material-symbols-outlined">train</span>
-						<?php echo $third_section['nearest-train-station']; ?>
-					</p>
+					<p><span class="material-symbols-outlined">location_city</span><?php echo $third_section['nearest-city']; ?></p>
+					<p><span class="material-symbols-outlined">flight</span><?php echo $third_section['nearest-airport']; ?></p>
+					<p><span class="material-symbols-outlined">directions_car</span><?php echo $third_section['nearest-road-distance']; ?></p>
+					<p><span class="material-symbols-outlined">train</span><?php echo $third_section['nearest-train-station']; ?></p>
 				</div>
 				<!-- google maps link -->
 				<div>
 					<img src="<?php echo esc_url($third_section['image-3']['url']); ?>" alt="<?php echo esc_attr($third_section['image-3']['alt']); ?>">
-					<p>
-						<?php echo $third_section['get-to-prompt']; ?>
-					</p>
-					<a href="<?php echo esc_url($third_section['google-maps-link']); ?>" target="_blank">Get Me
-						There<img src="<?php echo get_template_directory_uri(); ?>/images/google-maps.png" alt=""></a>
+					<p><?php echo $third_section['get-to-prompt']; ?></p>
+					<a href="<?php echo esc_url($third_section['google-maps-link']); ?>" target="_blank">Get Me There<img src="<?php echo get_template_directory_uri(); ?>/images/google-maps.png" alt=""></a>
 				</div>
 			</div>
 		</div>
